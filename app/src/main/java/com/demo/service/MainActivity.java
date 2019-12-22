dc8b0c1973a54552a5c40ff9e58c11761d9429a5
Vipul Kumar Singh <vipul24.9669@gmail.com>
2019-12-22 19:50:17 +0530

    Local Binding

--- app/src/main/java/com/demo/service/MainActivity.java
+++ app/src/main/java/com/demo/service/MainActivity.java
@@ -2,16 +2,32 @@ package com.demo.service;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button startServiceBtn;
     private Button stopServiceBtn;
+    private Button bindServiceBtn;
+    private Button unbindServiceBtn;
+    private Button getRandomNumberBtn;
+
+    private TextView textViewThreadCount;
+
+    private MyService myService;
+
+    private boolean isServiceBound;
+
+    private ServiceConnection serviceConnection;
 
     private Intent serviceIntent;
 
@@ -24,6 +40,11 @@ public class MainActivity extends AppCompatActivity {
 
         startServiceBtn = findViewById(R.id.serviceStartBtn);
         stopServiceBtn = findViewById(R.id.serviceStopBtn);
+        bindServiceBtn= findViewById(R.id.serviceBindBtn);
+        unbindServiceBtn = findViewById(R.id.serviceUnbindBtn);
+        getRandomNumberBtn= findViewById(R.id.getRandomNumberBtn);
+
+        textViewThreadCount = findViewById(R.id.randomNumberText);
 
         serviceIntent = new Intent(getApplicationContext(), MyService.class);
 
@@ -39,6 +60,50 @@ public class MainActivity extends AppCompatActivity {
                 Log.i("Button Pressed", "Stop Button Pressed");
                 stopService(serviceIntent);
                 break;
+            case R.id.serviceBindBtn:
+                bindService();
+                break;
+            case R.id.serviceUnbindBtn:
+                unbindService();
+                break;
+            case R.id.getRandomNumberBtn:
+                setRandomNumber();
+                break;
+        }
+    }
+
+    private void bindService() {
+        if (serviceConnection==null){
+            serviceConnection = new ServiceConnection() {
+                @Override
+                public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+                    MyService.MyServiceBinder myServiceBinder = (MyService.MyServiceBinder) iBinder;
+                    myService = myServiceBinder.getService();
+                    isServiceBound = true;
+                }
+
+                @Override
+                public void onServiceDisconnected(ComponentName componentName) {
+                    isServiceBound = false;
+                }
+            };
+        }
+
+        bindService(serviceIntent, serviceConnection, Context.BIND_AUTO_CREATE);//BIND_AUTO_CREATE : it'll create the service if not created at the time of binding
+    }
+
+    private void unbindService() {
+        if (isServiceBound){
+            unbindService(serviceConnection);
+            isServiceBound = false;
+        }
+    }
+
+    private void setRandomNumber() {
+        if (isServiceBound){
+            textViewThreadCount.setText("Random no. : "+ myService.getRandomNumber());
+        }else {
+            textViewThreadCount.setText("Service not Bound");
         }
     }
 }
--- app/src/main/java/com/demo/service/MyService.java
+++ app/src/main/java/com/demo/service/MyService.java
@@ -2,35 +2,89 @@ package com.demo.service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
 
+import java.util.Random;
+
 public class MyService extends Service {
+
+    private int randomNumber;
+    private boolean isRandomGeneratorOn;
+
+    private final int MIN = 0;
+    private final int MAX = 100;
+
+    class MyServiceBinder extends Binder{
+        public MyService getService(){
+            return MyService.this;
+        }
+    }
+
+    private IBinder binder = new MyServiceBinder();
 
     //gets executed whenever you start a a service
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.i(getString(R.string.service_demo), "Service Started, thread id: " + Thread.currentThread().getId());
-
-//        //to automatically destroy service once the task has been done
-//        stopSelf();  // calls onDestroy method
-
-        return super.onStartCommand(intent, flags, startId);
+        isRandomGeneratorOn = true;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                startRandomGenerator();
+            }
+        }).start();
+        return START_STICKY;
     }
 
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        Log.i(getString(R.string.service_demo), "In onBind");
+        return binder;
+    }
+
+    @Override
+    public void onRebind(Intent intent) {
+        super.onRebind(intent);
+        Log.i(getString(R.string.service_demo), "In onRebind");
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log.i(getString(R.string.service_demo), "In onUnbind");
+        return super.onUnbind(intent);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        stopRandomGenerator();
         Log.i(getString(R.string.service_demo), "Service Destroyed");
     }
 
+    private void startRandomGenerator() {
+        while (isRandomGeneratorOn) {
+            try {
+                Thread.sleep(1000);
+                if (isRandomGeneratorOn) {
+                    randomNumber = new Random().nextInt(MAX) + MIN;
+                    Log.i(getString(R.string.service_demo), "Thread id: " + Thread.currentThread().getId() + ", Random Number: " + randomNumber);
+                }
+            } catch (InterruptedException e) {
+                Log.e(getString(R.string.service_demo), "Thread Interrupted");
+            }
+        }
+    }
 
+    private void stopRandomGenerator() {
+        isRandomGeneratorOn = false;
+    }
+
+    public int getRandomNumber() {
+        return randomNumber;
+    }
 }
